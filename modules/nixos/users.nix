fbd6b6afc926aea444bb747e95aa52f9aa09c32f
Simon <simon@nixos.local>
2026-09-14 22:30:16 +0200

    enable fish shell system-wide

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -2,10 +2,13 @@
 # Paper for who lives in the house - from configuration.nix:70
 { config, pkgs, ... }:
 {
+  programs.fish.enable = true;
+
   users.users."simon" = {
     isNormalUser = true;
     description = "Simon";
     extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.fish;
     packages = with pkgs; [];
   };
 }
